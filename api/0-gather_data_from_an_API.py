afb4f15ff500f61b5d618dfb3b5917e37fcdd770
Mariahseggebruch <mariahseggebruch@gmail.com>
2023-07-20 09:38:16 -0700

    task 1 update, hopefully fixing putting an S in for spaces

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-"""
-Write a Python script that, using this REST API, for a
-given employee ID, returns information about his/her TODO
-list progress.
-"""
+'''returns information about his/her TODO list progress.'''
 import requests
-import sys
-if __name__ == "__main__":
-    # Retrieve employee data
-    employee_id = int(sys.argv[1])
-    employee_data = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
-    EMPLOYEE_NAME = employee_data["name"]
+from sys import argv
 
-    # Retrieve todo list for employee
-    adress = "https://jsonplaceholder.typicode.com/todos?userId="
-    todos_data = requests.get(
-        f"{adress}{employee_id}").json()
-    # Calculate progress
-    TOTAL_NUMBER_OF_TASKS = len(todos_data)
-    NUMBER_OF_DONE_TASKS = len(
-        [task for task in todos_data if task["completed"] is True])
-    progress = f"{NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}"
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+    user_id = argv[1]
+    response = requests.get(
+        f'{url}/users/{user_id}/todos',
+        params={'_expand': 'user'}
+    )
 
-    # Print progress and completed tasks
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks({progress}):")
-    for TASK_TITLE in todos_data:
-        if TASK_TITLE["completed"] is True:
-            print(f"\t {TASK_TITLE['title']}")
+    if response.status_code == 200:
+        data = response.json()
+        name = data[0]['user']['name']
+        tasks_ok = [task for task in data if task['completed']]
+        n_task_ok = len(tasks_ok)
+        total_task = len(data)
+
+        first_str = f"Employee {name} is done with tasks"
+
+        print(f"{first_str}({n_task_ok}/{total_task}):")
+        for task in tasks_ok:
+            print(f"\t {task['title']}")
+
+    else:
+        print(f"Error: {response.status_code}")
